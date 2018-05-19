9a7e89a29f88f7756a9ffd90976a73c05f472ef5
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-05-19 16:50:23 +0300

    finish 19 lecture

---
+++ 19_window_object/app.js
@@ -0,0 +1,56 @@
+// WINDOWS METHODS / OBJECTS / PROPERTIES
+
+// Alert
+// alert('Hello world');
+
+// Prompt
+// const str = prompt();
+// alert(str);
+
+// Confirm
+// if(confirm('Are you sure?')) {
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+// window.location.href = 'http://google.com';
+// Reload
+// window.location.reload;
+
+// History Object
+// window.history.go(-2);
+// val = window.history.length;
+
+// Navigator object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+console.log(val);
