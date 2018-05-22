08e6a71524ac441b9b65c8f9e56e289e055dcb8c
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-05-22 14:48:26 +0300

    finish 29 lecture

---
+++ 29_mouse_events/app.js
@@ -0,0 +1,30 @@
+const clearBtn = document.querySelector('.clear-tasks');
+const card = document.querySelector('.card');
+const heading = document.querySelector('h5');
+
+// Click
+// clearBtn.addEventListener('click', runEvent);
+// Double click
+// clearBtn.addEventListener('dblclick', runEvent);
+// Mouse down
+// clearBtn.addEventListener('mousedown', runEvent);
+// Mouse up
+// clearBtn.addEventListener('mouseup', runEvent);
+// Mouse enter
+// card.addEventListener('mouseenter', runEvent);
+// Mouse leave
+// card.addEventListener('mouseleave', runEvent);
+// Mouse over
+// card.addEventListener('mouseover', runEvent);
+// Mouse out
+// card.addEventListener('mouseout', runEvent);
+// Mouse move
+card.addEventListener('mousemove', runEvent);
+
+// Event handler
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  heading.textContent = `Mouse offsetX: ${e.offsetX} Mouse offsetY: ${e.offsetY}`;
+  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 65)`;
+}
